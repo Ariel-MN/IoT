b50d3b83c8575be3d23e42d23d746e5e125e8d07
Ariel Montes <arielmontes1989@gmail.com>
2020-04-16 12:06:04 +0200

    add templates, static and urls configuration

--- server/main/settings.py
+++ server/main/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = [
+    "web/static",
+]
 
 # Application definition
 
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Managing media
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
--- server/web/views.py
+++ server/web/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def home(request):
+    return render(request, 'home.html')

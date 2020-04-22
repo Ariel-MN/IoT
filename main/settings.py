e45f3a921b7e75d681f064f5d292667d63832b04
Ariel Montes <arielmontes1989@gmail.com>
2020-04-22 18:53:43 +0200

    heroku deployment

--- main/settings.py
+++ main/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
